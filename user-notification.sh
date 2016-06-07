ec746e56a0b0f2ba442abf3fffd7176bd383856c
Henrik Farre <henrik@bellcom.dk>
2016-06-07 15:10:14 +0200

    Increase timeout to 10 secs (missing 0)

--- user-notification.sh
+++ user-notification.sh
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 ICON=${1:-"emblem-generic"}
 TITLE="${2:-"Title"}"
 MESSAGE=${3:-"Message"}
-TIME_OUT=${4:-1000}
+TIME_OUT=${4:-10000}
 
 for USER_UID in /run/user/*; do
   sudo -u \#"${USER_UID##*/}" DISPLAY=:0.0 /usr/bin/notify-send -i "${ICON}" "${TITLE}" "${MESSAGE}" -t "${TIME_OUT}"
