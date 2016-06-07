ffb93b35cff3df7e152c0a63d323fd4301167e45
Henrik Farre <henrik@bellcom.dk>
2016-06-07 15:09:25 +0200

    Use sudo to run notify-send as correct user

--- user-notification.sh
+++ user-notification.sh
@@ -3,13 +3,14 @@
 set -o nounset
 set -o errexit
 set -o pipefail
+set -o xtrace
 IFS=$'\n\t'
 
 ICON=${1:-"emblem-generic"}
 TITLE="${2:-"Title"}"
 MESSAGE=${3:-"Message"}
-DISPLAY=":0.0"
+TIME_OUT=${4:-1000}
 
-for DBUS_SESSION in /run/user/*/bus; do
-  DBUS_SESSION_BUS_ADDRESS="unix:path=${DBUS_SESSION}" /usr/bin/notify-send -i "${ICON}" "${TITLE}" "${MESSAGE}"
+for USER_UID in /run/user/*; do
+  sudo -u \#"${USER_UID##*/}" DISPLAY=:0.0 /usr/bin/notify-send -i "${ICON}" "${TITLE}" "${MESSAGE}" -t "${TIME_OUT}"
 done
